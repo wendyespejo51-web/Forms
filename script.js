1fe345149559b20a3d418edc6e262d2c154b8a98
Catastro <wendyespejo51@gmail.com>
2025-10-09 11:05:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1113,7 +1113,7 @@ document.getElementById("enviarBtn").addEventListener("click", async function (e
   const datos = {};
   const campos = [
     "fecha", "grupo", "codigo", "alim", "Celda", "potencia", "cableNormalizado",
-    "cableSustraido", "seccionCable", "tipoCable", "ternasExistentes", "ternasFaltantes",
+    "cableSustraido", "seccionCable", "tipoCable", "MaterialCC","Cablemixto","ternasExistentes", "ternasFaltantes",
     "RoboReciente", "decoloracion", "carga1", "carga2", "carga3", "todosCables",
     "estadoTrafo", "EmpaqueTrafo", "ArandelaPresion", "TorqueBushing", "BorneBandera",
     "tapon", "Mirilla", "filtracion", "NivelFiltracion", "dondeFiltracion", "Estadoaceite",
@@ -1122,7 +1122,7 @@ document.getElementById("enviarBtn").addEventListener("click", async function (e
     
   // ===== VALIDAR CAMPOS OBLIGATORIOS =====
   const camposObligatorios = [ "fecha", "grupo", "codigo", "alim", "Celda", "potencia", "cableNormalizado",
-    "cableSustraido", "seccionCable", "tipoCable", "ternasExistentes", "ternasFaltantes",
+    "cableSustraido", "seccionCable", "tipoCable", "MaterialCC","Cablemixto", "ternasExistentes", "ternasFaltantes",
     "RoboReciente", "decoloracion", "todosCables", "estadoTrafo", "tapon", "sensacion"];
   const faltantes = [];
 
@@ -1131,7 +1131,7 @@ document.getElementById("enviarBtn").addEventListener("click", async function (e
   const elemento = document.getElementById(campo); 
   if (elemento) { 
     // Solo los campos realmente numéricos
-    if (["potencia", "carga1", "carga2", "carga3","MedicionTemperatura"].includes(campo)) { 
+    if (["potencia", "carga1", "carga2", "carga3"].includes(campo)) { 
       datos[campo] = elemento.value ? parseInt(elemento.value) : ""; 
     } else { 
       datos[campo] = elemento.value.trim() || ""; 
@@ -1195,6 +1195,7 @@ document.getElementById("enviarBtn").addEventListener("click", async function (e
   btn.disabled = false;
   btn.innerText = "Enviar";
 });
+
 
 
 
